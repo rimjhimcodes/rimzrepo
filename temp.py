da820d14f72684e127f7e7c9693788010b25ebb7
rimjhimcodes <rimjhim@uchicago.edu>
2022-03-31 15:43:39 -0500

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+this_is_repetitive = True
+if this_is_repetitive:
+    print("Boring")
+
+X = 10
+Y = 20
+sum(X+Y)
